7a6114779f1347b219456dd66e06da1ce8b1c418
Alloh Nkefor Rodrick <allohnkeforrodrick@gmail.com>
2024-06-07 04:02:01 +0100

    Dealing wth some new python here

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""An annotated-type function add that takes floats a and 
+b as arguments and returns their sum as a float.
+"""
+def add(a: float, b: float) -> float:
+    """Add two floats"""
+    return (a + b)
